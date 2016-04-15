4a48f26d7afa55f6a88b7cb03318c3716fb52ce4
Luke A Chase <chase.luke.a@gmail.com>
2016-04-15 15:46:44 -0500

    Created new button graphics in Photoshop, added atlas, added button states and actions

--- website/js/script.js
+++ website/js/script.js
@@ -1,34 +1,88 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
-var energyBar;
-var timer;
-var energyTotal = 10;
-var playGameOneButton;
-var playGameTwoButton;
-var playGameThreeButton;
-
+var energyBar,
+    energyTotal = 10;
+var timerText,
+    time;
+var buttons,
+    button_playWoodGame,
+    button_playMetalGame,
+    button_playStoneGame;
     
+
 function preload() {
-        game.load.image('shovel', '../assets/sprites/shovel.png');
+        //game.load.image('shovel', '../assets/sprites/shovel.png');
+    game.load.atlas('buttons', 'assets/sprites/buttons.png', 'assets/sprites/buttons.json');
 }
+
     
 function create() {
         
     game.stage.backgroundColor = '#34a26f';
     
+    button_playWoodGame = game.add.button(
+        game.width * 0.39,
+        game.height * 0.92,
+        'buttons',
+        playWoodGame,
+        this,
+        'btn_wood_over.png',
+        'btn_wood.png',
+        'btn_wood_down.png'
+    );
+    button_playWoodGame.anchor.set(0.5);
+    button_playWoodGame.scale.set(0.35);
+    
+    button_playMetalGame = game.add.button(
+        game.width * 0.50,
+        game.height * 0.90,
+        'buttons',
+        playMetalGame,
+        this,
+        'btn_metal_over.png',
+        'btn_metal.png',
+        'btn_metal_down.png'
+    );
+    button_playMetalGame.anchor.set(0.5);
+    button_playMetalGame.scale.set(0.35);
+    
+    button_playStoneGame = game.add.button(
+        game.width * 0.61,
+        game.height * 0.92,
+        'buttons',
+        playStoneGame,
+        this,
+        'btn_stone_over.png',
+        'btn_stone.png',
+        'btn_stone_down.png'
+    );
+    button_playStoneGame.anchor.set(0.5);
+    button_playStoneGame.scale.set(0.35);
+    
+    
     energyBar = game.add.text(game.world.centerX - 100, 20, 'Energy Left: 10', {fontSize: '20px', fill: '#000', align: 'center'});
     
     game.time.events.loop(Phaser.Timer.SECOND * 5, updateEnergyBar, this);
-    
-    playGameOneButton = game.add.button(150, 500, 'shovel', playGameOne, this);
-    playGameTwoButton = game.add.button(350, 500, 'shovel', playGameTwo, this);
-    playGameThreeButton = game.add.button(550, 500, 'shovel', playGameThree, this);
-        
 }
-    
+
+
+function playWoodGame() {
+    alert("Play wood game!");
+}
+
+function playMetalGame() {
+    alert("Play metal game!");
+}
+
+function playStoneGame() {
+    alert("Play stone game!");
+}
+
+
 function update() {
     
 }
+
 
 function updateEnergyBar() {
     if (energyTotal <= 9 ) {
